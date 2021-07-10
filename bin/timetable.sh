a1eb9f7402c5396371b1c3d75536d46c4a374416
Satvik Saha <sahasatvik@gmail.com>
2021-07-10 10:56:34 +0530

    Refined timetable notifier

--- bin/timetable.sh
+++ bin/timetable.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
 
-# The ~/bin/timetable/ diretory contains the files day.1 through day.7, which store
-# the event data for the weekdays Monday through Sunday.
-# The rows are of the following form (the last entry is the meet code)
-# time,title,description,abc-defg-hij
+# Events are placed in ~/bin/timetable/daily.x files, where 'x' varies
+# from 1-7 denoting Monday-Sunday.
+# Each of these files is written with lines in the following format:
+#
+#       event time,title,description,google meet code
+#
+# The description, google meet code are optional.
+# The 'event time' is in simple HH:MM format.
 
-tablefile="$HOME/bin/timetable/day.$(date +%u)"
+tablefile="$HOME/bin/timetable/daily.$(date +%u)"
 
 cat $tablefile | awk -F',' '{printf "<span color=\"#777777\">%6s</span>%8s  <span color=\"#777777\">%-50s</span> %16s\n", $1, $2, $3, $4}'
--- bin/timetable_notify.sh
+++ bin/timetable_notify.sh
@@ -1,23 +1,44 @@
 #!/usr/bin/env sh
 
-# Notifies upcoming events in the ~/bin/timetable/day.x files.
+# Notifies upcoming events in the ~/bin/timetable/daily.x files, plus
+# the more general ~/bin/timetable/calendar file. Note that all of these
+# files must be present, so you may want to run the following commands
+# to create them.
+#
+#       $ mkdir -p ~/bin/timetable
+#       $ touch ~/bin/timetable/calendar ~/bin/timetable/daily.{1..7}
+#
+# Each of these files is written with lines in the following format:
+#
+#       event time,title,description,google meet code (or additional info)
+#
+# The description, google meet code are optional. Note that 'event time'
+# must be in a valid 'date --date=' string format. For example, '14:30'
+# (2:30 pm on the current day) or '16:45 January 11 2020' are valid.
+# The daily.x files usually have 'event time' in the simple HH:MM format,
+# while the calendar file mentions the full time and date.
+
 # Run as a timed systemd service.
 
 SECONDS_BEFORE=300
 
 currentdate=$(date +%s)
-tablefile="$HOME/bin/timetable/day.$(date +%u)"
+tablefile="$HOME/bin/timetable/daily.$(date +%u)"
+calendarfile="$HOME/bin/timetable/calendar"
+cachefile="$HOME/bin/timetable/.cache"
 
-touch "$HOME/bin/timetable/.cache"
-cache="$(cat $HOME/bin/timetable/.cache)"
+touch "$cachefile"
+cache="$(cat $cachefile)"
 
 IFS=,
-while read line; do
+cat "$tablefile" "$calendarfile" | while read line; do
 
         [[ -z "$line" ]] && continue
 
-        read hhmm title description meetcode <<< $line
-        eventdate=$(date --date=$hhmm +%s)
+        read etime title description meetcode <<< $line
+        eventdate="$(date --date=$etime +%s)"
+
+        echo $eventdate $title $description
 
         [[ ! -z $(echo "$cache" | grep "$eventdate$title") ]] && continue
         [[ $eventdate -lt $currentdate ]] && continue
@@ -25,9 +46,11 @@ while read line; do
         diff=$(($eventdate-$currentdate))
         [[ $diff -gt "$SECONDS_BEFORE" ]] && continue
 
+
+        hhmm="$(date --date=$etime +%H:%M)"
         notify-send -u critical \
                 "$title at $hhmm" "$description\n<span foreground='gray'>$meetcode</span>" \
                 -h string:x-canonical-private-synchronous:"$hhmm $title" && \
-        echo "$eventdate$title" >> $HOME/bin/timetable/.cache
+        echo "$eventdate$title" >> $cachefile
 
-done < $tablefile
+done
