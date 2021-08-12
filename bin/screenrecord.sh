bdb175353e60d4d3d51950eeb36c8378c9c57308
Satvik Saha <sahasatvik@gmail.com>
2021-08-12 11:57:31 +0530

    Updated for use with USB-C earphones

--- bin/screenrecord.sh
+++ bin/screenrecord.sh
@@ -13,11 +13,15 @@ windowflags="-video_size 1920x1080 -i :0.0"
 vcodecflags="-c:v ffv1 -preset ultrafast"
 acodecflags=""
 
+DESKTOP_AUDIO="alsa_output.usb-bestechnic_Samsung_USB_C_Earphone_20160406.1-00.analog-stereo.monitor"
+MICROPHONE="alsa_input.usb-bestechnic_Samsung_USB_C_Earphone_20160406.1-00.mono-fallback"
+
 PANEL_FIFO=/tmp/panel-fifo
 
 stop_recording() {
         trap - SIGTERM
         kill -SIGINT $pid 2>/dev/null
+        pacmd set-default-source 0
         notify-send -t 2000 -u low "Recording stopped"
         echo "Rprocessing" > "$PANEL_FIFO"
         ffmpeg -i "$filemkv" -af 'lowpass=3000, highpass=200, afftdn=tn=enabled' -c:v libx264 -c:a aac "$filempg"
@@ -27,12 +31,12 @@ stop_recording() {
 
 for i in "$@"; do
         if [[ "$i" == "microphone" ]]; then
-                pacmd set-default-source alsa_input.pci-0000_00_1f.3.analog-stereo
+                pacmd set-default-source "$MICROPHONE"
                 audioflags="-f alsa -ac 2 -i default"
                 acodecflags="-c:a pcm_s16le"
                 mode="$mode\nMicrophone audio"
         elif [[ "$i" == "desktop" ]]; then
-                pacmd set-default-source alsa_output.pci-0000_00_1f.3.analog-stereo.monitor
+                pacmd set-default-source "$DESKTOP_AUDIO"
                 audioflags="-f alsa -ac 2 -i default"
                 acodecflags="-c:a pcm_s16le"
                 mode="$mode\nDesktop audio"
