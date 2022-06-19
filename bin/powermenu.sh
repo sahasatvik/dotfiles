d06fed48ecfaf9903eb36241b440531b1d3654f1
Satvik Saha <sahasatvik@gmail.com>
2022-06-19 23:23:29 +0530

    Added powermenu

---
+++ bin/powermenu.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+opt_lock=""
+opt_sleep=""
+opt_logout=""
+opt_reboot="累"
+opt_poweroff="襤"
+
+get_option() {
+        echo -e "$opt_poweroff\n$opt_reboot\n$opt_lock\n$opt_sleep\n$opt_logout" \
+                | rofi -dmenu -config ~/.config/rofi/powermenu.rasi -selected-row 2
+}
+
+option="$(get_option)"
+
+[[ -z "$option" ]] && exit
+
+case "$option" in
+        $opt_lock)
+                dm-tool lock
+                ;;
+        $opt_sleep)
+                systemctl suspend
+                ;;
+        $opt_logout)
+                bspc quit
+                ;;
+        $opt_reboot)
+                systemctl reboot
+                ;;
+        $opt_poweroff)
+                systemctl poweroff
+                ;;
+esac
