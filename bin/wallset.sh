37796d9014257e63f32ae87b18942eb57371670f
Satvik Saha <sahasatvik@gmail.com>
2022-06-30 15:28:07 +0530

    Make wallset.sh update the 'current' wallpaper file

--- bin/wallset.sh
+++ bin/wallset.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env sh
 
 [[ -z "$1" ]] && exit 1;
+[[ ! -f "$1" ]] && exit 1;
 
 WALL_DIR="$HOME/Pictures/wallpapers/main"
 WALL_BLUR_DIR="$HOME/Pictures/wallpapers/blur"
 WALL_CURRENT="$HOME/Pictures/wallpapers/current"
 
 hsetroot -cover "$1" &> /dev/null
+echo "$1" > "$WALL_CURRENT"
+
 wall_basename=$(basename -- "$1")
 wall_blur="$WALL_BLUR_DIR/$wall_basename"
 
@@ -14,4 +17,5 @@ if [[ ! -f "$wall_blur" ]]; then
         convert "$1" -brightness-contrast -6x0 -blur 0x4 "$wall_blur"
 fi
 cp "$wall_blur" /etc/lightdm/background.jpg
+
 
