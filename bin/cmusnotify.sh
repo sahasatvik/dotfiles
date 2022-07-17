bbae3a76ceac55745b60e91acc0dfc5104aef350
Satvik Saha <sahasatvik@gmail.com>
2022-07-17 12:12:24 +0530

    Check if cmus is playing; tweaked colors

--- bin/cmusnotify.sh
+++ bin/cmusnotify.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 status="$(cmus-remote -C status)"
+[[ -z "$status" ]] && exit 1
+
 playing=$(echo "$status" | grep ^status | sed 's/^status //g')
 
 heading=""
@@ -17,8 +19,10 @@ file=$(echo "$status" | grep '^file' | sed 's/^file //g')
 title=$(echo "$status" | grep '^tag title' | sed 's/^tag title //g')
 artist=$(echo "$status" | grep '^tag artist' | sed 's/^tag artist //g')
 
+[[ -z "$file" ]] && exit 1
+
 notify-send -u low \
         -i $(~/bin/musicthumbnail.sh "$file") \
         -h string:x-canonical-private-synchronous:cmus \
         "$heading" \
-        "$artist\n$title"
+        "<span color='#81a2be'>$artist</span>\n<span color='#aeafad'>$title</span>"
