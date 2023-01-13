d5ffe93f091756aa15e3f7a04bf08779b54a34d7
Satvik Saha <sahasatvik@gmail.com>
2023-01-13 15:44:57 +0530

    Changed power boundaries

--- bin/battery.sh
+++ bin/battery.sh
@@ -3,7 +3,7 @@
 percent=$(acpi | sed -E 's/.*[^0-9]([0-9]+)%.*/\1/g')
 if [[ $percent -gt 50 ]]; then
         echo "$percent%"
-elif [[ $percent -gt 25 ]]; then
+elif [[ $percent -gt 30 ]]; then
         echo "<b><span foreground='#cd9a76'>$percent%</span></b>"
 elif [[ $percent -gt 15 ]]; then
         echo "<b><span foreground='#ffaa55'>$percent%</span></b>"
