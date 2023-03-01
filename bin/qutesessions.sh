ba7b8d5cf272f96dfc03436bd20ebd51aa817d8b
Satvik Saha <sahasatvik@gmail.com>
2023-03-01 22:17:10 +0530

    Added qutebrowser session menu

---
+++ bin/qutesessions.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+list_sessions() {
+        fd --type file .yml $HOME/.local/share/qutebrowser/sessions \
+                | sed 's#.*/##g' \
+                | sed 's#.yml$##g' \
+                | grep -vx "_autosave" \
+                | sort
+}
+
+get_session() {
+        list_sessions \
+                | rofi -dmenu -markup-rows -i -p 'find'
+}
+
+session="$(get_session)"
+
+[[ -z "$session" ]] && exit
+
+qutebrowser -r "$session" &
