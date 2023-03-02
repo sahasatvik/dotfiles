b9ee79b732eea9d917e73b5ef26aaff46d9fabba
Satvik Saha <sahasatvik@gmail.com>
2023-03-02 18:48:50 +0530

    Added wifi/bluetooth to status

--- bin/status.sh
+++ bin/status.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
 muted="$($HOME/bin/muted.sh)"
+wifi="$($HOME/bin/wificonnected.sh)"
+blue="$($HOME/bin/bluetoothconnected.sh)"
+
 mutetext=""
 if [[ "$muted" == "muted" ]]; then
         mutetext="(<span foreground='#cc6666'>muted</span>)"
@@ -9,4 +12,13 @@ fi
 echo "<b>Volume</b>     : $($HOME/bin/volume.sh) $mutetext"
 echo "<b>Brightness</b> : $($HOME/bin/brightness.sh)"
 echo "<b>Battery</b>    : $($HOME/bin/battery.sh)"
+
+
+if [[ ! -z "$wifi" ]]; then
+        echo "<b>Network</b>    : $wifi"
+fi
+if [[ ! -z "$blue" ]]; then
+        echo "<b>Bluetooth</b>  : $blue"
+fi
+
 echo "<b>Workspaces</b> : $($HOME/bin/workspaces.sh)"
