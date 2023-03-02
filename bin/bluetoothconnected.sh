c2c1c5e8ffc8b8cbbff2a9cfca74cce198d5f90f
Satvik Saha <sahasatvik@gmail.com>
2023-03-02 21:42:30 +0530

    Show battery percentage

--- bin/bluetoothconnected.sh
+++ bin/bluetoothconnected.sh
@@ -14,8 +14,18 @@ uuid_to_device() {
                 | sed "s/.*$1\s*\(.*\)$/\1/g"
 }
 
+battery_percentage() {
+        bluetoothctl info "$1" \
+                | grep -e "Battery Percentage: .*(.*)" \
+                | sed "s/.*(\(.*\))/\1/g"
+}
+
 uuids | while read uuid; do
         if [[ "$(is_connected $uuid)" ]]; then
-                uuid_to_device "$uuid"
+                device="$(uuid_to_device $uuid)"
+                if [[ "$(battery_percentage $uuid)" ]]; then
+                        battery=" ($(battery_percentage $uuid)%)"
+                fi
+                echo "$device$battery"
         fi
 done
